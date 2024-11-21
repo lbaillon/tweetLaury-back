e3804262123856bf9d1170d2c05a083a230dd8d9
Laury Baillon <laury-baillon@hotmail.fr>
2024-11-21 10:49:54 +0100

    signin route

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,9 @@
-const uid2 = require('uid2');
-const bcrypt = require('bcrypt');
 var express = require('express');
 var router = express.Router();
 require('../models/connexion');
+
+const uid2 = require('uid2');
+const bcrypt = require('bcrypt');
 
 const User = require("../models/users");
 const { checkBody } = require('../modules/checkBody');
@@ -24,11 +25,26 @@ router.post('/signup', async (req, res) => {
         password: hash,
         token: uid2(32)
     })
-    newUser.save().then(newDoc => {
-        res.json({result: true, user: newDoc})
-    })
+      const newDoc = await newUser.save()
+      res.json({result: true, user: newDoc})
+    
     }
   } 
 })
+
+router.post('/signin', async (req, res) => {
+  const data = await User.findOne({username: req.body.username})
+  if (checkBody(req.body, ['username', 'password'])) {
+    if(! req.body.username|| !req.body.password){
+      res.json({ result: false, error: 'Missing or empty fields'})
+      return
+    }else if (data && bcrypt.compareSync(req.body.password, data.password)){
+      res.json({ result: true, token: data.token });
+    }else {
+      res.json({ result: false, error: 'User not found or wrong password' });
+    }
+  }
+})
+
 
 module.exports = router;

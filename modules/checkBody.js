3672e1abb61e1cdf5d73201514a89f03a5c73754
Laury Baillon <laury-baillon@hotmail.fr>
2024-11-21 10:28:03 +0100

    mise en place route signup

---
+++ modules/checkBody.js
@@ -0,0 +1,18 @@
+
+
+
+// function to check inputs in username and password
+
+function checkBody(body, keys) {
+    let result = true
+    for (const input of keys){
+        console.log(body[input])
+        if(!body[input] || body[input]=== ""){
+            result = false
+        }
+    }
+    return result
+}
+
+
+module.exports = {checkBody}
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,34 @@
+const uid2 = require('uid2');
+const bcrypt = require('bcrypt');
 var express = require('express');
 var router = express.Router();
+require('../models/connexion');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+const User = require("../models/users");
+const { checkBody } = require('../modules/checkBody');
+
+router.post('/signup', async (req, res) => {
+  const username = await User.findOne({ username : req.body.username})
+  if (checkBody(req.body, ['username', 'firstname', 'password'])) {
+    if(! req.body.username|| !req.body.password){
+      res.json({ result: false, error: 'Missing or empty fields'})
+      return
+    }else if( username !== null){
+      res.json({result: false, error: 'User already exists'})
+      return
+    }else{
+      const hash = bcrypt.hashSync(req.body.password, 10);
+      const newUser = new User({
+        username: req.body.username,
+        firstname: req.body.firstname,
+        password: hash,
+        token: uid2(32)
+    })
+    newUser.save().then(newDoc => {
+        res.json({result: true, user: newDoc})
+    })
+    }
+  } 
+})
 
 module.exports = router;
